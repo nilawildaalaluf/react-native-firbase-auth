52c58a12b62c212b670b0503eaf61ed64c7c8036
_nilawilda <nila@cocowork.co>
2019-02-27 13:49:07 +0700

    firebase analytics react native

---
+++ App.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import firebase from 'react-native-firebase';
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+
+export default class App extends Component {
+  render() {
+    firebase.analytics().setCurrentScreen('App');
+    firebase.analytics().logEvent('openApp');
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={() =>
+              firebase.analytics().logEvent('buttonPressed')
+            }>
+          <Text style={styles.welcome}>Welcome to React Native!</Text>
+        </TouchableOpacity>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
